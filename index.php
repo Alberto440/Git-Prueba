fb150f25984850ca4225d286a620360cd9d50d7f
alumno <alumno@A14107>
2020-01-27 10:55:14 +0100

    Parte de comit Parte 2 d comit

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
     print "Este sería en primer bloque en despliegue";
     print "Este sería en segundo bloque en despliegue";
     print "Este sería en tercero bloque en despliegue";
-   
+    
+     print "Este sería en cuarto bloque en despliegue";
+      print "Este sería en quinto bloque en despliegue";   
 ?>
