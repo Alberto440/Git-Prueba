48114620dbe8d89f414f16aeddca63ee1fcb4489
alumno <alumno@A14107>
2020-01-27 10:53:59 +0100

    Parte de comit

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php
+
+    print "Este sería en primer bloque en despliegue";
+    print "Este sería en segundo bloque en despliegue";
+    print "Este sería en tercero bloque en despliegue";
+   
+?>
